af65599d16242371b1160fac316613673ecf93c6
h.mohtashami <mohtashami911@gmail.com>
2023-05-27 16:09:34 +0330

    main

--- src/Screens/Auth/index.tsx
+++ src/Screens/Auth/index.tsx
@@ -1,47 +1,44 @@
-
-import React, { useContext, useEffect } from 'react';
-import { Platform, Text, View } from 'react-native';
-import { SignInAppleComponent } from './SignInApple';
-import { SignInGoogleComponent } from './SignInGoogle';
+import React, {useContext, useEffect} from 'react';
+import {Platform, Text, View} from 'react-native';
+import {SignInAppleComponent} from './SignInApple';
+import {SignInGoogleComponent} from './SignInGoogle';
 import styles from './style';
-// @ts-ignore 
-import LoginBanner from "../../../assets/svg/LoginBanner.svg";
-import { validateToken } from '../../Services/auth.service';
-import { AuthContext } from '../../Contexts/AuthContext';
-import { setAuthorizationToken } from '../../Services/Api/instance';
+// @ts-ignore
+import LoginBanner from '../../../assets/svg/LoginBanner.svg';
+import {validateToken} from '../../Services/auth.service';
+import {AuthContext} from '../../Contexts/AuthContext';
+import {setAuthorizationToken} from '../../Services/Api/instance';
+import {Notifications, Registered} from 'react-native-notifications';
 
-
-export function AuthScreen({ navigation }: any) {
+export function AuthScreen({navigation}: any) {
   const authContext = useContext(AuthContext);
   useEffect(() => {
+    Notifications.events().registerRemoteNotificationsRegistered(
+      (event: Registered) => {
+        console.log('Device Token Received', event.deviceToken);
+      },
+    );
     navigation.setOptions({
       title: '',
       headerStyle: {
         backgroundColor: '#FF597B',
       },
       headerShadowVisible: false,
-    })
-
-  }, [navigation])
-
-
+    });
+  }, [navigation]);
 
   const onValidateToken = (token, email, token_type) => {
-    console.log(token, email, token_type);
-    validateToken({ token, token_type }).then(
-      (jwtToken) => {
-        authContext.setAuthState({
-          accessToken: token,
-          authenticated: true,
-        })
-        setAuthorizationToken(jwtToken.data);
-        authContext.setGenericPasswordSignIn(email, jwtToken.data)
-      }
-    )
-  }
+    validateToken({token, token_type}).then(jwtToken => {
+      authContext.setAuthState({
+        accessToken: token,
+        authenticated: true,
+      });
+      setAuthorizationToken(jwtToken.data);
+      authContext.setGenericPasswordSignIn(email, jwtToken.data);
+    });
+  };
 
   return (
-
     <View style={styles.mainContainer}>
       <View style={styles.logoContainer}>
         <LoginBanner width={200} height={300} />
@@ -49,15 +46,11 @@ export function AuthScreen({ navigation }: any) {
       </View>
 
       <View style={styles.SignInOptionContainer}>
-
-        {Platform.OS === 'ios' &&
-          <SignInAppleComponent />
-        }
-        {Platform.OS === 'android' &&
+        {Platform.OS === 'ios' && <SignInAppleComponent />}
+        {Platform.OS === 'android' && (
           <SignInGoogleComponent onSubmit={onValidateToken} />
-        }
+        )}
       </View>
     </View>
-
   );
 }
--- src/Services/Api/instance.ts
+++ src/Services/Api/instance.ts
@@ -1,35 +1,33 @@
-import axios, { AxiosError } from "axios";
-import { ErrorHandeling } from "./error-handling";
+import axios, {AxiosError} from 'axios';
+import {ErrorHandeling} from './error-handling';
 
 export const axiosInstance = axios.create({
-    baseURL: "https://api.lovedateapp.com",
+  baseURL: 'https://api.lovedateapp.com',
 });
 
-
 export const setAuthorizationToken = (token: string | null) => {
-    axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-}
+  axiosInstance.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
 
 axiosInstance.interceptors.request.use(
-    function (config) {
-        const requestConfig = { ...config };
-        console.log('auth', requestConfig.headers['Authorization'])
-        requestConfig.headers!['Content-Type'] = 'application/json';
-        requestConfig.headers!['Accept'] = 'application/json';
-        return requestConfig;
-    },
-    function (error) {
-        // Do something with request error
-        return Promise.reject(error);
-    }
+  function (config) {
+    const requestConfig = {...config};
+    requestConfig.headers!['Content-Type'] = 'application/json';
+    requestConfig.headers!.Accept = 'application/json';
+    return requestConfig;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  },
 );
 
 axiosInstance.interceptors.response.use(
-    function (response) {
-        return response;
-    },
-    function (error: AxiosError) {
-        ErrorHandeling(error.response!)
-        return Promise.reject(error);
-    }
+  function (response) {
+    return response;
+  },
+  function (error: AxiosError) {
+    ErrorHandeling(error.response!);
+    return Promise.reject(error);
+  },
 );
